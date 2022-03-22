6f649d4bb7764d78ddd0cc5123551325c6ffd91a
FloCorvalan <florencia.corvalan@usach.cl>
2022-03-22 01:01:05 -0300

    Prueba de imagen

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <img :src="image">
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "PruebaImagen",
+  components: {},
+  data: function () {
+    return {
+      image: null,
+    };
+  },
+  created() {
+    this.displayImage()
+  },
+  methods: {
+    displayImage() {
+      axios.get("http://localhost:8081/prueba/image/").then((response) => {
+        console.log(response);
+        try {
+          this.image =
+            "data:image/jpeg;base64," +
+            btoa(
+              new Uint8Array(response.data).reduce(
+                (data, byte) => data + String.fromCharCode(byte),
+                ""
+              )
+            );
+        } catch (err) {
+          console.log(err);
+        }
+      });
+    },
+  },
+};
+</script>
+
