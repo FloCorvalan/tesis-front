ba6602627eb379b49b12ab46beae752f65f8307f
FloCorvalan <florencia.corvalan@usach.cl>
2022-03-30 01:27:49 -0300

    Se ve la imagen

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,6 +7,7 @@
 
 <script>
 import axios from "axios";
+import {Buffer} from "buffer"
 
 export default {
   name: "PruebaImagen",
@@ -17,7 +18,7 @@ export default {
     };
   },
   created() {
-    this.displayImage()
+    this.prueba()
   },
   methods: {
     displayImage() {
@@ -37,6 +38,37 @@ export default {
         }
       });
     },
+    getTeamParticipation(){
+      axios.get("http://127.0.0.1:5001/jenkins/team-participation").then((response) => {
+        console.log(response.data[0].username);
+      });
+    }, 
+    displayImage2(){
+      axios.get("http://127.0.0.1:5001/process-model/get-last-image/6241fad36d714f635bafbc9f", {
+      responseType: 'blob',
+    }).then((response) => {
+        console.log(response);
+        //let res1 = response.data
+        //let blob = new Blob ([res1], {
+        //type: "image/png"
+        //});
+        console.log(typeof(response.data))
+        var x = Buffer.from(response.data, 'binary')
+        console.log(x)
+        var y = x.toString('base64')
+        console.log(y)
+        this.image = "data:image/png;base64,".concat(y)
+        //console.log(this.image)
+      });
+    },
+    prueba(){
+      axios.get("http://127.0.0.1:5001/process-model/get-last-image/6241fad36d714f635bafbc9f", {
+      responseType: 'blob',
+    }).then((response) => {
+        var objectURL = URL.createObjectURL(response.data);
+        this.image = objectURL;
+      });
+    }
   },
 };
 </script>
