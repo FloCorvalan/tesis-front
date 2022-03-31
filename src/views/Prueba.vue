ffb2982bea5a8de1c56b2d09de29d9ee8b16cab6
FloCorvalan <florencia.corvalan@usach.cl>
2022-03-31 02:42:08 -0300

    Se ve el bpmn pero pequeño

--- src/views/Prueba.vue
+++ src/views/Prueba.vue
@@ -1,20 +1,18 @@
 <template>
   <div>
-    <img :src="image">
-    <div id="bpmn"></div>
+    <img :src="image" />
+    <div id="canvas"></div>
   </div>
 </template>
 <style >
-
-#bpmn .djs-container{
+#bpmn .bjs-container {
   position: absolute;
 }
-
 </style>
 <script>
 import axios from "axios";
-import {Buffer} from "buffer"
-import BpmnJS from 'bpmn-js';
+import { Buffer } from "buffer";
+import BpmnJS from "bpmn-js";
 
 export default {
   name: "PruebaImagen",
@@ -26,8 +24,8 @@ export default {
     };
   },
   created() {
-    this.prueba()
-    this.pruebados()
+    this.prueba();
+    this.pruebados();
   },
   methods: {
     displayImage() {
@@ -47,63 +45,98 @@ export default {
         }
       });
     },
-    getTeamParticipation(){
-      axios.get("http://127.0.0.1:5001/jenkins/team-participation").then((response) => {
-        console.log(response.data[0].username);
-      });
-    }, 
-    displayImage2(){
-      axios.get("http://127.0.0.1:5001/process-model/get-last-image/6241fad36d714f635bafbc9f", {
-      responseType: 'blob',
-    }).then((response) => {
-        console.log(response);
-        //let res1 = response.data
-        //let blob = new Blob ([res1], {
-        //type: "image/png"
-        //});
-        console.log(typeof(response.data))
-        var x = Buffer.from(response.data, 'binary')
-        console.log(x)
-        var y = x.toString('base64')
-        console.log(y)
-        this.image = "data:image/png;base64,".concat(y)
-        //console.log(this.image)
-      });
+    getTeamParticipation() {
+      axios
+        .get("http://127.0.0.1:5001/jenkins/team-participation")
+        .then((response) => {
+          console.log(response.data[0].username);
+        });
     },
-    prueba(){
-      axios.get("http://127.0.0.1:5001/process-model/get-last-image/6241fad36d714f635bafbc9f", {
-      responseType: 'blob',
-    }).then((response) => {
-        var objectURL = URL.createObjectURL(response.data);
-        this.image = objectURL;
-      });
-    }, 
-    pruebados(){
+    displayImage2() {
       axios
         .get(
-          "https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/master/colors/resources/pizza-collaboration.bpmn"
+          "http://127.0.0.1:5001/process-model/get-last-image/6241fad36d714f635bafbc9f",
+          {
+            responseType: "blob",
+          }
         )
+        .then((response) => {
+          console.log(response);
+          //let res1 = response.data
+          //let blob = new Blob ([res1], {
+          //type: "image/png"
+          //});
+          console.log(typeof response.data);
+          var x = Buffer.from(response.data, "binary");
+          console.log(x);
+          var y = x.toString("base64");
+          console.log(y);
+          this.image = "data:image/png;base64,".concat(y);
+          //console.log(this.image)
+        });
+    },
+    prueba() {
+      axios
+        .get(
+          "http://127.0.0.1:5001/process-model/get-last-image/6241fad36d714f635bafbc9f",
+          {
+            responseType: "blob",
+          }
+        )
+        .then((response) => {
+          var objectURL = URL.createObjectURL(response.data);
+          this.image = objectURL;
+        });
+    },
+    pruebados() {
+      axios
+        .get("http://127.0.0.1:5001/process-model/get-bpmn")
         .then((r) => {
           this.diagram = r.data;
           //console.log(this.diagram);
           const xml = this.diagram; // my BPMN 2.0 xml
           const viewer = new BpmnJS({
-            container: '#bpmn', 
-            position: 'absolute'
+            container: "#canvas",
+            position: "absolute",
+            propertiesPanel: {
+              parent: "#js-properties-panel",
+            },
           });
-          console.log(viewer)
+          console.log(viewer);
           try {
-            viewer.importXML(xml);
-            viewer._container('#container')
-            console.log('rendered');
+            viewer.importXML(xml, function () {
+              var canvas = viewer.get("canvas");
+
+              canvas.zoom("fit-viewport");
+              var eventBus = viewer.get("eventBus");
+
+              // you may hook into any of the following events
+              var events = [
+                //"element.hover",
+                //"element.out",
+                "element.click",
+                //"element.dblclick",
+                //"element.mousedown",
+                //"element.mouseup",
+              ];
+
+              events.forEach(function (event) {
+                eventBus.on(event, function (e) {
+                  // e.element = the model element
+                  // e.gfx = the graphical element
+
+                  console.log(event, "on", e.element.id);
+                });
+              });
+            });
           } catch (err) {
-            console.log('error rendering', err);
+            console.log("error rendering", err);
           }
         })
         .catch((e) => {
           console.log(e);
         });
-    }
+    },
   },
 };
 </script>
