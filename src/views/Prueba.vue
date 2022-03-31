a0ead5031853b375628a706d40e3fd91215fb3c9
FloCorvalan <florencia.corvalan@usach.cl>
2022-03-31 09:54:41 -0300

    ZoomScroll y minimap

--- src/views/Prueba.vue
+++ src/views/Prueba.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
     <img :src="image" />
-    <div id="canvas"></div>
+    <div class="container" v-on:click="setViewboxCenteredAroundPoint">
+      <div id="canvas"></div>
+
+      <div id="js-properties-panel" class="panel"></div>
+    </div>
+    <div>
+      <button v-on:click="zoomOut">zoom out</button>
+      <button v-on:click="zoomIn">zoom in</button>
+    </div>
   </div>
 </template>
 <style >
-#bpmn .bjs-container {
-  position: absolute;
-}
+@import url("diagram-js-minimap/assets/diagram-js-minimap.css");
+@import url("bpmn-js/dist/assets/diagram-js.css");
+@import url("bpmn-js/dist/assets/bpmn-font/css/bpmn.css");
 </style>
 <script>
 import axios from "axios";
 import { Buffer } from "buffer";
 import BpmnJS from "bpmn-js";
+import minimapModule from "diagram-js-minimap";
+import ZoomScrollModule from 'diagram-js/lib/navigation/zoomscroll';
 
 export default {
   name: "PruebaImagen",
@@ -21,6 +31,7 @@ export default {
     return {
       image: null,
       diagram: null,
+      viewer: null,
     };
   },
   created() {
@@ -98,11 +109,11 @@ export default {
           const viewer = new BpmnJS({
             container: "#canvas",
             position: "absolute",
-            propertiesPanel: {
-              parent: "#js-properties-panel",
-            },
+            display: "flex",
+            additionalModules: [minimapModule, ZoomScrollModule],
           });
           console.log(viewer);
+          this.viewer = viewer;
           try {
             viewer.importXML(xml, function () {
               var canvas = viewer.get("canvas");
@@ -126,6 +137,33 @@ export default {
                   // e.gfx = the graphical element
 
                   console.log(event, "on", e.element.id);
+                  viewer.get("minimap").open();
+                  var canvas = document.getElementById("canvas");
+                  console.log(canvas)
+                  var context = canvas.getContext("2d");
+                  var scale = 1;
+                  var originx = 0;
+                  var originy = 0;
+                  canvas.onmousewheel = function (event) {
+                    var mousex = event.clientX - canvas.offsetLeft;
+                    var mousey = event.clientY - canvas.offsetTop;
+                    var wheel = event.wheelDelta / 120; //n or -n
+
+                    var zoom = 1 + wheel / 2;
+
+                    context.translate(originx, originy);
+                    context.scale(zoom, zoom);
+                    context.translate(
+                      -(mousex / scale + originx - mousex / (scale * zoom)),
+                      -(mousey / scale + originy - mousey / (scale * zoom))
+                    );
+
+                    originx =
+                      mousex / scale + originx - mousex / (scale * zoom);
+                    originy =
+                      mousey / scale + originy - mousey / (scale * zoom);
+                    scale *= zoom;
+                  };
                 });
               });
             });
@@ -136,6 +174,52 @@ export default {
         .catch((e) => {
           console.log(e);
         });
+    },
+    zoomIn() {
+      this.viewer.get("canvas").zoom(0.5);
+    },
+    setViewboxCenteredAroundPoint(point) {
+      var canvas = this.viewer.get("canvas");
+      // get cached viewbox to preserve zoom
+      var cachedViewbox = canvas.viewbox(),
+        cachedViewboxWidth = cachedViewbox.width,
+        cachedViewboxHeight = cachedViewbox.height;
+
+      canvas.viewbox({
+        x: point.x - cachedViewboxWidth / 2,
+        y: point.y - cachedViewboxHeight / 2,
+        width: cachedViewboxWidth,
+        height: cachedViewboxHeight,
+      });
+    },
+    zoomOut() {
+      var SCALE_FACTOR = 1.01;
+      var canvas = this.viewer.get("canvas");
+
+      canvas.height = canvas.height * (1 / SCALE_FACTOR);
+      canvas.width = canvas.width * (1 / SCALE_FACTOR);
+
+      var objects = canvas.objects;
+      for (var i in objects) {
+        var scaleX = objects[i].scaleX;
+        var scaleY = objects[i].scaleY;
+        var left = objects[i].left;
+        var top = objects[i].top;
+
+        var tempScaleX = scaleX * (1 / SCALE_FACTOR);
+        var tempScaleY = scaleY * (1 / SCALE_FACTOR);
+        var tempLeft = left * (1 / SCALE_FACTOR);
+        var tempTop = top * (1 / SCALE_FACTOR);
+
+        objects[i].scaleX = tempScaleX;
+        objects[i].scaleY = tempScaleY;
+        objects[i].left = tempLeft;
+        objects[i].top = tempTop;
+
+        objects[i].setCoords();
+      }
+
+      //canvas.save();
     },
   },
 };
