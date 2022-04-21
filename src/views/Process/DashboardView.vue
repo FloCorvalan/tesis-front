c577aed1733ef6284712a794406be926d2d8f0d2
FloCorvalan <florencia.corvalan@usach.cl>
2022-04-21 01:29:41 -0400

    Cambios

--- src/views/Process/DashboardView.vue
+++ src/views/Process/DashboardView.vue
@@ -10,15 +10,7 @@
           <v-card class="mx-1 mb-1">
             <v-card-title class="pa-6 pb-0">
               <v-row no-gutters>
-                <v-col
-                  cols="7"
-                  sm="4"
-                  md="4"
-                  lg="5"
-                  class="d-flex align-center"
-                >
-                  <p>Modelo de proceso BPMN equipo {{ team_name }}</p>
-                </v-col>
+                  <h3>Modelos de proceso BPMN equipo {{ team_name }}</h3>
               </v-row>
             </v-card-title>
             <v-card-text class="pa-6">
@@ -31,7 +23,7 @@
                         class="project-name"
                         v-on:click="changeVisibleModel(project)"
                       >
-                        Proyecto {{ project.name }}
+                        <h3>Proyecto {{ project.name }}</h3>
                       </div>
                     </li>
                   </ul>
@@ -49,7 +41,7 @@
         <v-col lg="4" sm="6" cols="12">
           <v-card class="mx-1 mb-1">
             <v-card-title class="pa-6 pb-3">
-              <p>Jira</p>
+              <h3>Jira</h3>
               <v-spacer></v-spacer>
               <v-menu>
                 <template v-slot:activator="{ on, attrs }">
@@ -59,11 +51,8 @@
                 </template>
                 <v-list>
                   <v-list-item
-                    v-for="(item, i) in mock.menu"
-                    :key="i"
-                    @click="() => {}"
                   >
-                    <v-list-item-title>{{ item }}</v-list-item-title>
+                    <v-list-item-title></v-list-item-title>
                   </v-list-item>
                 </v-list>
               </v-menu>
@@ -105,14 +94,6 @@
                           :series="project.jenkins_options.series"
                         >
                         </DonutChart>
-                        <!--ApexChart
-                  height="100%"
-                  width="100%"
-                  type="bar"
-                  v-if="apexLoading"
-                  :options="jenkins_options.options"
-                  :series="jenkins_options.series"
-                ></ApexChart-->
                       </v-row>
                       <v-row v-if="project.jenkins_options_bar != null">
                         <BarChart
@@ -120,14 +101,6 @@
                           :series="project.jenkins_options_bar.series"
                         >
                         </BarChart>
-                        <!--ApexChart
-                  height="100%"
-                  width="100%"
-                  type="bar"
-                  v-if="apexLoading"
-                  :options="jenkins_options.options"
-                  :series="jenkins_options.series"
-                ></ApexChart-->
                       </v-row>
                     </v-card-text>
                   </v-card>
@@ -176,163 +149,26 @@
             </ul>
           </v-card>
         </v-col>
-        <v-col lg="4" sm="6" cols="12">
-          <v-card class="mx-1 mb-1">
-            <v-card-title class="pa-6 pb-3">
-              <p>RNS</p>
-              <v-spacer></v-spacer>
-              <v-menu>
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn icon v-bind="attrs" v-on="on">
-                    <v-icon color="textColor">mdi-dots-vertical</v-icon>
-                  </v-btn>
-                </template>
-                <v-list>
-                  <v-list-item
-                    v-for="(item, i) in mock.menu"
-                    :key="i"
-                    @click="() => {}"
-                  >
-                    <v-list-item-title>{{ item }}</v-list-item-title>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-            </v-card-title>
-            <v-card-text class="pa-6 pt-0">
-              <v-row no-gutters>
-                <v-col cols="7" class="my-auto">
-                  <span class="" style="font-size: 42px"
-                    >175 <span class="error--text caption">-3.1%</span>
-                  </span>
-                </v-col>
-                <v-col cols="5">
-                  <ApexChart
-                    height="100"
-                    type="bar"
-                    v-if="apexLoading"
-                    :options="mock.apexBar3.options"
-                    :series="mock.apexBar3.series"
-                  ></ApexChart>
-                </v-col>
-              </v-row>
-              <v-row no-gutters class="justify-space-between">
-                <div>
-                  <div class="subtext">
-                    31 <v-icon color="error"> mdi-arrow-bottom-right</v-icon>
-                  </div>
-                  <div class="subtext-index">Registrations</div>
-                </div>
-                <div>
-                  <div class="subtext">
-                    3.23%<v-icon color="success"> mdi-arrow-top-right</v-icon>
-                  </div>
-                  <div class="subtext-index">Bounce Rate</div>
-                </div>
-                <div>
-                  <div class="subtext">
-                    301<v-icon color="success"> mdi-arrow-top-right</v-icon>
-                  </div>
-                  <div class="subtext-index">Views</div>
-                </div>
-              </v-row>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col cols="12">
-          <v-card class="support-requests mx-1 mb-1">
-            <v-card-title class="pa-6 pb-0">
-              <p>Support Requests</p>
-              <v-spacer></v-spacer>
-              <v-menu>
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn icon v-bind="attrs" v-on="on">
-                    <v-icon color="textColor">mdi-dots-vertical</v-icon>
-                  </v-btn>
-                </template>
-                <v-list>
-                  <v-list-item
-                    v-for="(item, i) in mock.menu"
-                    :key="i"
-                    @click="() => {}"
-                  >
-                    <v-list-item-title>{{ item }}</v-list-item-title>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-            </v-card-title>
-            <v-card-text class="pa-0">
-              <v-simple-table>
-                <template v-slot:default>
-                  <thead class="pl-2">
-                    <tr>
-                      <th class="text-left pa-6">NAME</th>
-                      <th class="text-left">EMAIL</th>
-                      <th class="text-left">PRODUCT</th>
-                      <th class="text-left">PRICE</th>
-                      <th class="text-left">DATE</th>
-                      <th class="text-left">CITY</th>
-                      <th class="text-left">STATUS</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr v-for="item in mock.table" :key="item.name">
-                      <td class="pa-6">{{ item.name }}</td>
-                      <td>{{ item.email }}</td>
-                      <td>{{ item.product }}</td>
-                      <td>{{ item.price }}</td>
-                      <td>{{ item.date }}</td>
-                      <td>{{ item.city }}</td>
-                      <td v-if="item.status === 'Sent'">
-                        <v-chip link color="success" class="ma-2 ml-0">
-                          Sent
-                        </v-chip>
-                      </td>
-                      <td v-else-if="item.status === 'Pending'">
-                        <v-chip link color="warning" class="ma-2 ml-0">
-                          Pending
-                        </v-chip>
-                      </td>
-                      <td v-else-if="item.status === 'Declined'">
-                        <v-chip link color="secondary" class="ma-2 ml-0">
-                          Declined
-                        </v-chip>
-                      </td>
-                    </tr>
-                  </tbody>
-                </template>
-              </v-simple-table>
-            </v-card-text>
-          </v-card>
-        </v-col>
       </v-row>
     </div>
   </v-container>
 </template>
 
 <script>
-import mock from "./mock";
-import ApexChart from "vue-apexcharts";
 import DonutChart from "@/components/DonutChart.vue";
 import BarChart from "@/components/BarChart.vue";
 import BPMNModel from "@/components/BPMNModel.vue";
 import axios from "axios";
-// MODELO
-/*import BpmnJS from "bpmn-js";
-import minimapModule from "diagram-js-minimap";
-import ZoomScrollModule from "diagram-js/lib/navigation/zoomscroll";
-import MoveCanvasModule from "diagram-js/lib/navigation/movecanvas";*/
 
 export default {
   name: "DashboardView",
   components: {
-    ApexChart,
     DonutChart,
     BarChart,
     BPMNModel,
   },
   data() {
     return {
-      mock,
       apexLoading: false,
       value: this.getRandomInt(10, 90),
       value2: this.getRandomInt(10, 90),
@@ -360,87 +196,6 @@ export default {
         project.visibleModel = false;
       }
     },
-    /*getBPMNs() {
-      if (this.projects != []) {
-        var i = 0;
-        while (i < this.projects.length) {
-          this.getProjectBPMN(this.projects[i].id);
-          i++;
-        }
-      }
-    },
-    //funcion para obtener un modelo bpmn
-    getProjectBPMN(team_project_id) {
-      axios
-        .get(
-          process.env.VUE_APP_BASE_URL +
-            "/process-model/get-last-bpmn/" +
-            team_project_id
-        )
-        .then((r) => {
-          this.diagram = r.data;
-          //console.log(this.diagram);
-          const xml = this.diagram; // my BPMN 2.0 xml
-          const viewer = new BpmnJS({
-            container: "#canvas",
-            position: "relative",
-            height: "100vh",
-            additionalModules: [
-              minimapModule,
-              ZoomScrollModule,
-              MoveCanvasModule,
-            ],
-          });
-          console.log(viewer);
-          this.viewer = viewer;
-          try {
-            var parser = new DOMParser();
-            var doc = parser.parseFromString(xml, "text/xml");
-            var x = doc.getElementsByName("CONSTRUIR");
-            console.log(x[0].id);
-            var el = doc.getElementById(x[0].id);
-            el.addEventListener("click", this.modifyText(), true);
-
-            viewer.importXML(xml, function () {
-              var canvas = viewer.get("canvas");
-              canvas.addMarker(x[0].id, "highlight");
-
-              canvas.zoom("fit-viewport");
-              viewer.get("minimap").open();
-              //console.log(viewer.get("minimap"));
-              //canvas.addMarker('task', 'highlight');
-              //var elementRegistry = viewer.get('elementRegistry');
-
-              /*var eventBus = viewer.get("eventBus");
-
-              // you may hook into any of the following events
-              var events = [
-                //"element.hover",
-                //"element.out",
-                "element.click",
-                //"element.dblclick",
-                //"element.mousedown",
-                //"element.mouseup",
-              ];
-
-              events.forEach(function (event) {
-                eventBus.on(event, function (e) {
-                  // e.element = the model element
-                  // e.gfx = the graphical element
-
-                  console.log(event, "on", e.element.id);
-                  //viewer.get("minimap").open();
-                });
-              });  //*
-            });
-          } catch (err) {
-            console.log("error rendering", err);
-          }
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    },*/
     //funcion para obtener los ids de proyectos de un equipo de desarrollo
     getProjects() {
       if (this.team_id != "") {
