0a64692a867122c93854c513b827a403210cc29d
FloCorvalan <florencia.corvalan@usach.cl>
2022-06-17 15:56:38 -0400

    fix sonarqube 1

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/prueba',
-    name: 'prueba',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Prueba.vue')
-  },
-  {
-    path: '/pruebachart',
-    name: 'pruebachart',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Charts/PruebaView.vue')
-  },
-  {
-    path: '/dashresp',
-    name: 'dashresp',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Process/DashResp.vue')
-  },
   ////////////////////////////////////
   ////////////// VISTAS //////////////
   ////////////////////////////////////
